87c62e063496948b75a5831d27399e3f0c6156f4
imabhishekc <abhishekchakraborty776@gmail.com>
2025-06-08 19:56:35 +0545

    Added custom exception test-set

--- py-basics/EXCEPTION_HANDLING/practice_set1_5.py
+++ py-basics/EXCEPTION_HANDLING/practice_set1_5.py
@@ -7,3 +7,22 @@ class TooSmallError(Exception):
 Write a program that raises TooSmallError if the input number is less than 10.
 """
 
+# Create a custom Exception class inheriting the python's Exception class
+class TooSmallError(Exception):
+    """Custom exception raised when the number is less than 10."""
+    pass
+
+try:
+    num = int(input("Enter a number (>=10): "))
+    if num < 10:
+        raise TooSmallError("Number is too small! It should be 10 or greater than 10.")
+    print("You entered: ", num)
+
+except TooSmallError as e:
+    print(e)
+
+except ValueError:
+    print("Please enter a valid number.")
+
+finally:
+    print("Input check complete.")
