513f6ea39e9880e306c7cbaca2490381677eeca3
imabhishekc <abhishekchakraborty776@gmail.com>
2025-06-16 19:47:06 +0545

    Added Linked list classes and methods

--- py-dsa/2_LinkedList/practice_set1_1.py
+++ py-dsa/2_LinkedList/practice_set1_1.py
@@ -6,3 +6,43 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr += str(itr.data) + '-->'
+            itr = itr.next
+
+        print(llstr)
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        
+        itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+    
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_values(['lion', 'tiger', 'leopard', 'jaguar', 'cheetah', 'black panther'])
+    ll.print()
