f16a93fe085a5865f7caba2a2c69747fee4be4cc
imabhishekc <abhishekchakraborty776@gmail.com>
2025-06-17 13:06:03 +0545

    Adding required methods in LinkedList class

--- py-dsa/2_LinkedList/practice_set1_1.py
+++ py-dsa/2_LinkedList/practice_set1_1.py
@@ -41,8 +41,67 @@ class LinkedList:
         for data in data_list:
             self.insert_at_end(data)
     
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+
+        return count
+
+    def remove_at(self, index):
+        if index < 0 or index >= self.get_length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count += 1
+
+    def insert_at(self, index, data):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index -1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insert_values(['lion', 'tiger', 'leopard', 'jaguar', 'cheetah', 'black panther'])
+    ll.insert_values(['lion', 'tiger', 'leopard'])
     ll.print()
+
+    ll.insert_at_beginning('panther')
+    ll.print()
+
+    ll.insert_at_end('cheetah')
+    ll.print()
+
+    ll.insert_at(2, 'cat')
+    ll.print()
+
+    ll.remove_at(3)
+    ll.print()
+
+    print("Length:", ll.get_length())
